4f470ad6c99ca827fcd05b8abf03be20139b4f26
Pictarin <kevin.adam@clear-it.de>
2017-09-09 14:09:15 +0200

    setup Launcher and Test

---
+++ src/main/java/de/pictarin/cookbook/application/CookbookLauncher.java
@@ -0,0 +1,13 @@
+package de.pictarin.cookbook.application;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CookbookLauncher {
+
+	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(CookbookLauncher.class);
+	    logger.info("Hello World");
+	}
+
+}
---
+++ src/test/java/de/pictarin/cookbook/application/CookbookLauncherTest.java
@@ -0,0 +1,15 @@
+package de.pictarin.cookbook.application;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.Test;
+
+public class CookbookLauncherTest {
+
+	@Test
+	public void test() {
+		assertThat(true, is(true));
+	}
+
+}
